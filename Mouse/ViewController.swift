0700333a5f0b28c1450fca298ce8181e09e3b9bd
John Zhou <john-xyz@outlook.com>
2023-07-29 10:04:59 -0500

    Initial Commit

---
+++ Mouse/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Mouse
+//
+//  Created by John Zhou on 7/29/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
